de399a9b35ec47617f3f827da8a62c8f2e831972
Павел Артеменко <partyomenko@gmail.com>
2020-08-22 16:11:17 +0300

    Add event manager (#14)

---
+++ core/src/main/kotlin/darkmatter/event/Events.kt
@@ -0,0 +1,24 @@
+package darkmatter.event
+
+import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.utils.Pool
+
+
+sealed class GameEvent : Pool.Poolable
+
+object NonEntity : Entity()
+
+interface GameEventListener<T : GameEvent> {
+    fun onEvent(event: T)
+}
+
+/**
+ * Dummy test event
+ */
+@Suppress("UNUSED")
+data class DummyTestEvent(var player: Entity = NonEntity) : GameEvent() {
+
+    override fun reset() {
+        player = NonEntity
+    }
+}
