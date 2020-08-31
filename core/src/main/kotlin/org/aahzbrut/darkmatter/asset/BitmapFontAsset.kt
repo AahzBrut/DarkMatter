042b4583408bf23289bff4c6e87e7f03eb3e40c9
Павел Артеменко <partyomenko@gmail.com>
2020-08-31 03:04:45 -0700

    Add loading screen (#48)

--- core/src/main/kotlin/org/aahzbrut/darkmatter/asset/BitmapFontAsset.kt
+++ core/src/main/kotlin/org/aahzbrut/darkmatter/asset/BitmapFontAsset.kt
@@ -1,7 +1,10 @@
 package org.aahzbrut.darkmatter.asset
 
+import org.aahzbrut.darkmatter.asset.TextureAtlasAsset.*
+
 enum class BitmapFontAsset(private val asset: BaseBitmapFontAsset) {
-    SCORE_FONT(BaseBitmapFontAsset(fileName = "font.fnt"));
+    SCORE_FONT(BaseBitmapFontAsset(fileName = "font.fnt", fontAtlasName = TEXTURE_ATLAS.descriptor.fileName)),
+    UI_FONT(BaseBitmapFontAsset("ui", fileName = "GoodTiming.fnt", fontAtlasName = UI_ATLAS.descriptor.fileName));
 
     val descriptor
         get() = asset.descriptor
--- core/src/main/kotlin/org/aahzbrut/darkmatter/asset/TextureAtlasAsset.kt
+++ core/src/main/kotlin/org/aahzbrut/darkmatter/asset/TextureAtlasAsset.kt
@@ -1,7 +1,8 @@
 package org.aahzbrut.darkmatter.asset
 
 enum class TextureAtlasAsset(private val asset: BaseAtlasAsset) {
-    TEXTURE_ATLAS(BaseAtlasAsset(fileName = "textures.atlas"));
+    TEXTURE_ATLAS(BaseAtlasAsset(fileName = "textures.atlas")),
+    UI_ATLAS(BaseAtlasAsset("ui", "ui.atlas"));
 
     val descriptor
         get() = asset.descriptor
